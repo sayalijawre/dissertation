60ff4851ba3b9ffd2dcfe2c479b28c5cba6eb4d8
sayaliaj <sjawre@deloitte.com>
2023-05-26 19:19:54 +0530

    Final code change

--- skinDetector.py
+++ skinDetector.py
@@ -3,12 +3,14 @@ from enum import Enum, auto
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.stats import f_oneway
 
 
 class Method(Enum):
     '''Available methods for processing an image
                     REGION_BASED: segment the skin using the HSV and YCbCr colorspaces, followed by the Watershed algorithm'''
     REGION_BASED = auto()
+
 
 class SkinDetector():
 
@@ -43,7 +45,7 @@ class SkinDetector():
         lower_YCbCr_values = np.array((0, 138, 67), dtype="uint8")
         upper_YCbCr_values = np.array((255, 173, 133), dtype="uint8")
 
-        # A binary mask is returned. White pixels (255) represent pixels that fall into the upper/lower.
+        # A binary mask is returned. White skin_reshape (255) represent skin_reshape that fall into the upper/lower.
         mask_YCbCr = cv2.inRange(
             YCbCr_image, lower_YCbCr_values, upper_YCbCr_values)
         mask_HSV = cv2.inRange(HSV_image, lower_HSV_values, upper_HSV_values)
@@ -79,16 +81,67 @@ class SkinDetector():
         '''Show all processed images concatenated along the 1 axis using imshow '''
         rgb_mask = cv2.cvtColor(self.image_mask, cv2.COLOR_GRAY2RGB)
         cv2.namedWindow(window_title, cv2.WINDOW_NORMAL)
+
+        # Convert to HSV
+        hsv_img = cv2.cvtColor(self.skin, cv2.COLOR_BGR2HSV)
+
+        # Increase hue and saturation
+        hue_shift = 15
+        saturation_scale = 1.5
+        hsv_img[:, :, 0] = (hsv_img[:, :, 0] + hue_shift) % 180
+        hsv_img[:, :, 1] = hsv_img[:, :, 1] * saturation_scale
+
+        # Convert back to BGR
+        yellow_face = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
+
+        # Display result
         cv2.imshow(window_title, np.concatenate(
-            (self.image, rgb_mask, self.skin), axis=1))
+            (self.image, rgb_mask, self.skin, yellow_face), axis=1))
+
+        # Skin detected image histogram
+        plt.subplot(1, 2, 1)
         blue_color = cv2.calcHist([self.skin], [0], None, [256], [1, 256])
-        red_color = cv2.calcHist([self.skin], [1], None, [256], [1, 256])
-        green_color = cv2.calcHist([self.skin], [2], None, [256], [1, 256])
-        # plt.plot(abc)
+        green_color = cv2.calcHist([self.skin], [1], None, [256], [1, 256])
+        red_color = cv2.calcHist([self.skin], [2], None, [256], [1, 256])
+
         plt.plot(blue_color, color="blue")
         plt.plot(green_color, color="green")
         plt.plot(red_color, color="red")
-        
+
+        # Yello skin image histogram
+        plt.subplot(1, 2, 2)
+        blue_color = cv2.calcHist([yellow_face], [0], None, [256], [1, 256])
+        green_color = cv2.calcHist([yellow_face], [1], None, [256], [1, 256])
+        red_color = cv2.calcHist([yellow_face], [2], None, [256], [1, 256])
+
+        plt.plot(blue_color, color="blue")
+        plt.plot(green_color, color="green")
+        plt.plot(red_color, color="red")
+
+        # reshape images
+        skin_reshape = self.skin.reshape(-1, 3)
+        yellow_skin_reshape = yellow_face.reshape(-1, 3)
+
+        # Separate the pixel values for each RGB channel
+        blue_channel_skin = skin_reshape[:, 0]
+        blue_channel_yellow_skin = yellow_skin_reshape[:, 0]
+        green_channel_skin = skin_reshape[:, 1]
+        green_channel_yellow_skin = yellow_skin_reshape[:, 1]
+        red_channel_skin = skin_reshape[:, 2]
+        red_channel_yellow_skin = yellow_skin_reshape[:, 2]
+
+        # Perform ANOVA test for each channel
+        _, red_pvalue = f_oneway(red_channel_skin, red_channel_yellow_skin)
+        _, green_pvalue = f_oneway(
+            green_channel_skin, green_channel_yellow_skin)
+        _, blue_pvalue = f_oneway(blue_channel_skin, blue_channel_yellow_skin)
+
+        # Print the p-values
+        print("Red channel p-value:", red_pvalue)
+        print("Green channel p-value:", green_pvalue)
+        print("Blue channel p-value:", blue_pvalue)
+
+       
         plt.show()
         cv2.waitKey(0)
 
@@ -100,6 +153,6 @@ class SkinDetector():
 imageName = sys.argv[1]
 
 detector = SkinDetector(
-    image_path="C:/Users/sjawre/Desktop/onboarding/Personal/Dissertattion/Indian/resize/00000046_resized.png")
+    image_path="./Negroids/Negroids/00000293_resized.png")
 detector.find_skin()
 detector.show_all_images()
